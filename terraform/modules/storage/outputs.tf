34478ef6fda5d2cce66e1dd218ae5686515034dd
unknown <kcy9442@gmail.com>
2026-08-10 12:33:18 +0900

    feat(ci): deploy Flutter web to CloudFront

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -3,6 +3,11 @@ output "bucket_name" {
   value       = aws_s3_bucket.static_site.id
 }
 
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID for cache invalidation"
+  value       = aws_cloudfront_distribution.static_site.id
+}
+
 output "website_endpoint" {
   description = "CloudFront HTTPS 웹 사이트 URL"
   value       = "https://${aws_cloudfront_distribution.static_site.domain_name}"
