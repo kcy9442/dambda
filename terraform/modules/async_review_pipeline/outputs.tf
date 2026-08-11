8c7e0a97c648bc68401f6ef272b1bc332d922691
unknown <kcy9442@gmail.com>
2026-08-11 09:57:13 +0900

    feat(events): route approved reviews through EventBridge

--- terraform/modules/async_review_pipeline/outputs.tf
+++ terraform/modules/async_review_pipeline/outputs.tf
@@ -2,3 +2,5 @@ output "queue_url" { value = aws_sqs_queue.review_events.id }
 output "queue_arn" { value = aws_sqs_queue.review_events.arn }
 output "queue_name" { value = aws_sqs_queue.review_events.name }
 output "state_machine_arn" { value = aws_sfn_state_machine.review_moderation.arn }
+output "event_bus_arn" { value = aws_cloudwatch_event_bus.review_events.arn }
+output "event_bus_name" { value = aws_cloudwatch_event_bus.review_events.name }
